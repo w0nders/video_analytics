41eb2dc470c081a46fc80d2f8d0700f369e0816f
Ubuntu <linux-admin@shadowsocks.dc5vifi2aguevplsvuzuit0rrf.ax.internal.cloudapp.net>
2019-08-22 10:20:56 +0000

    second modify

--- aws/start_face_detection.sh
+++ aws/start_face_detection.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-aws rekognitio nstart-face-detection \
---viedo S3Object={Bucket=string,Name=string,Version=string}\
+aws rekognition start-face-detection \
+--video "S3Object={Bucket=novikovtst,Name=video_store/10floor-Video_2.mp4}" \
+--notification-channel "SNSTopicArn=arn:aws:sns:eu-west-1:382425229343:AmazonRekognition,RoleArn=arn:aws:iam::382425229343:role/novikovtst" \
+--job-tag "start-face-search"
+
