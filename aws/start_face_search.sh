aec282766c9eeebdb20bf1b6845ef22380e0fac4
Ubuntu <linux-admin@shadowsocks.dc5vifi2aguevplsvuzuit0rrf.ax.internal.cloudapp.net>
2019-08-22 10:29:35 +0000

    change collection_id to "collection2"

--- aws/start_face_search.sh
+++ aws/start_face_search.sh
@@ -4,7 +4,7 @@ aws rekognition start-face-search \
 --video "S3Object={Bucket=novikovtst,Name=video_store/10floor-Video_2.mp4}" \
 --notification-channel "SNSTopicArn=arn:aws:sns:eu-west-1:382425229343:AmazonRekognition,RoleArn=arn:aws:iam::382425229343:role/novikovtst" \
 --job-tag "start-face-search" \
---collection-id "MyCollection2" \
+--collection-id "collection2" \
 --face-match-threshold "80"
 
 
